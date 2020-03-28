e40e089f22d24c6f19191aaafa9ca0a0d2032216
IndiaSmile :) <dev@indiasmile.org>
2020-03-28 17:51:50 +0530

    add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,10 @@
   .wrapper
     transition(name="fade")
       nuxt
+    .colophon
+      .colophon__content
+        p By the अति विशिष्ट people at <a href="https://nataco.us/">NATACO</a>
+        p जनहित में जारी &copy; 2020
 </template>
 
 <script>
@@ -32,7 +36,7 @@ export default {
     -webkit-animation-name fadeIn
     animation-name fadeIn
 
-
+// fade page animation
 @-webkit-keyframes fadeIn
   from
     opacity 0
@@ -44,4 +48,14 @@ export default {
     opacity 0
   to
     opacity 1
+
+// colophon
+.colophon
+  background #34495e
+  font-size .75em
+  padding 2em 0
+  margin 1.5em 0 0
+  text-align center
+  color rgba(white, .65)
+  line-height 2.5
 </style>
