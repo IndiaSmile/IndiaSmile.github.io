0eed3621fa9769bc89aaad357a8e61c4fe492aff
IndiaSmile :) <dev@indiasmile.org>
2020-03-28 23:50:25 +0530

    thriller ratings

--- pages/entertainment/movies/english/thriller.vue
+++ pages/entertainment/movies/english/thriller.vue
@@ -38,6 +38,42 @@ export default {
           rating: 8.6,
           buttons: [
             {
+              type: 'Google',
+              link: 'https://play.google.com/store/movies/details/The_Silence_of_the_Lambs?id=sHqrhjNCmTo&hl=en',
+            },
+          ],
+        },
+        {
+          custom: 'Sharer',
+        },
+        {
+          heading: 'Seven',
+          text: `A dark and gripping thriller which saves its most shocking revelations for the final moments of the movie! Based on two detectives, who try to run down a gruesome serial killer who bases his murders on the seven deadly sins. Will surely get your heart pumping!`,
+          rating: 8.6,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/950149',
+            },
+          ],
+        },
+        {
+          heading: 'A Simple Favor',
+          text: `A stylish post-modern film noir directed by Paul Feig, based around Stephanie who seeks to uncover the truth behind her best friend’s sudden disappearance from their small town. It is a thriller, carries suspense and is packed with an uneasy but mild humour at its core!`,
+          rating: 6.8,
+          buttons: [
+            {
+              type: 'Google',
+              link: 'https://play.google.com/store/movies/details/A_Simple_Favor?id=7A59F3987EFA280AMV&hl=en',
+            },
+          ],
+        },
+        {
+          heading: 'Shutter Island',
+          text: `Two US marshals, are sent to an asylum on a remote island in order to investigate the disappearance of a patient, where Teddy uncovers a shocking truth about the place. Oh what can we say about this classic! The film's primary effect is on the senses. Everything is brought together into a disturbing pool of secrets.`,
+          rating: 8.1,
+          buttons: [
+            {
               type: 'Netflix',
               link: 'https://foo.com',
             },
@@ -48,47 +84,109 @@ export default {
           ],
         },
         {
-          custom: 'Sharer',
-        },
-        {
-          heading: 'Seven',
-          text: `A dark and gripping thriller which saves its most shocking revelations for the final moments of the movie! Based on two detectives, who try to run down a gruesome serial killer who bases his murders on the seven deadly sins. Will surely get your heart pumping!`,
-        },
-        {
-          heading: 'A Simple Favor',
-          text: `A stylish post-modern film noir directed by Paul Feig, based around Stephanie who seeks to uncover the truth behind her best friend’s sudden disappearance from their small town. It is a thriller, carries suspense and is packed with an uneasy but mild humour at its core!`,
-        },
-        {
-          heading: 'Shutter Island',
-          text: `Two US marshals, are sent to an asylum on a remote island in order to investigate the disappearance of a patient, where Teddy uncovers a shocking truth about the place. Oh what can we say about this classic! The film's primary effect is on the senses. Everything is brought together into a disturbing pool of secrets.`,
-        },
-        {
           heading: 'No Country for Old Men',
           text: `A hunter's life takes a drastic turn when he is pursued by a psychopathic killer who wants the money he acquired! Powerful and moving performances from the cast, having won an Oscar for the best film!`,
+          rating: 8.1,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://foo.com',
+            },
+            {
+              type: 'Prime',
+              link: 'https://baz.com',
+            },
+          ],
         },
         {
           heading: 'Oldboy (2003, Korean)',
           text: `A man, held captive for no apparent reason for years, is given a cell phone, money and expensive clothes and released. Unless he finds out the identity of his captor, an even worse fate awaits him. This one is for the strong hearted and can be considered a real mind bender!`,
+          rating: 8.4,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://foo.com',
+            },
+            {
+              type: 'Prime',
+              link: 'https://baz.com',
+            },
+          ],
         },
         {
           heading: 'Mystic River',
           text: `After the death of a girl, a police officer, investigates her murder which leads him to a crime that took place twenty-five years ago. A back in time thriller with an unprecedented set of events that turn your mind topsy turvy in search for the truth!`,
+          rating: 7.9,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://foo.com',
+            },
+            {
+              type: 'Prime',
+              link: 'https://baz.com',
+            },
+          ],
         },
         {
           heading: 'Gone Girl',
           text: `Nick discovers that the entire media focus has shifted on him when his wife disappears on the day of their fifth wedding anniversary. This is a sick film, and also brilliant.`,
+          rating: 8.1,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://foo.com',
+            },
+            {
+              type: 'Prime',
+              link: 'https://baz.com',
+            },
+          ],
         },
         {
           heading: 'Memento',
           text: `An insurance investigator suffers from anterograde amnesia and uses notes and tattoos to hunt for the man he thinks killed his wife, which is the last thing he remembers! One of the most original and unique thrillers that Hollywood has seen in its prime!`,
+          rating: 8.4,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://foo.com',
+            },
+            {
+              type: 'Prime',
+              link: 'https://baz.com',
+            },
+          ],
         },
         {
           heading: 'The Gift',
           text: `A married couple run into an old acquaintance. Things take a turn when he begins to drop in unannounced at their house and inundates them with mysterious gifts. This resourceful and edgy thriller marks another string in the talented director Joel Edgerton’s palette.`,
+          rating: 7,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://foo.com',
+            },
+            {
+              type: 'Prime',
+              link: 'https://baz.com',
+            },
+          ],
         },
         {
           heading: 'Bird Box',
           text: `When a mysterious force decimates the world, only one thing is certain, if you see it, you die. Facing the unknown, Malorie finds love, hope and a new beginning and must flee with her two children down a treacherous river to the one place left that may offer sanctuary. Sandra Bullock stars and finds praise for her performance in this thrilling contest of life.`,
+          rating: 6.6,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://foo.com',
+            },
+            {
+              type: 'Prime',
+              link: 'https://baz.com',
+            },
+          ],
         },
       ],
     }
