63274c27ab96fd6db2dfe7e0a7d5e8fc2c3ad016
IndiaSmile :) <dev@indiasmile.org>
2020-03-29 01:40:07 +0530

    fix back button

--- components/BackButton.vue
+++ components/BackButton.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-	button.hero__button(@click="goBack")
-		span.hero__button__icon
-		| BACK
+  button.hero__button(@click="goBack")
+    span.hero__button__icon
+    | BACK
 
 </template>
 
@@ -13,7 +13,8 @@ export default {
       const path = window.location.pathname.split('/')
       if (path.length >= 1) {
         path.pop()
-        return this.$router.push({ path: path.join('/') })
+        const backPath = path.join('/') || '/'
+        return this.$router.push({ path: backPath })
       }
     },
   },
@@ -22,24 +23,24 @@ export default {
 
 <style lang="stylus" scoped>
 .hero__button
-	position absolute
-	left 1.125rem
-	height 1.5rem
-	font-size 0.75em
-	font-weight bold
-	border 0
-	color #3B4CFE
-	background-color rgba(59, 76, 254, 0.2)
-	border-radius 2.5rem
-	z-index 100
-	padding-right 0.625rem
-	display flex
-	align-items center
+  position absolute
+  left 1.125rem
+  height 1.5rem
+  font-size 0.75em
+  font-weight bold
+  border 0
+  color #3B4CFE
+  background-color rgba(59, 76, 254, 0.2)
+  border-radius 2.5rem
+  z-index 100
+  padding-right 0.625rem
+  display flex
+  align-items center
 
-	&__icon
-		display inline-block
-		border solid #1C5BFF
-		border-width 0 1.5px 1.5px 0
-		transform rotate(135deg)
-		padding 2px
+  &__icon
+    display inline-block
+    border solid #1C5BFF
+    border-width 0 1.5px 1.5px 0
+    transform rotate(135deg)
+    padding 2px
 </style>
