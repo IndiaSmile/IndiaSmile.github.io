e47ac9cd907a78f2b8b6332037cd973ad9e3e216
IndiaSmile :) <dev@indiasmile.org>
2020-03-29 01:17:22 +0530

    update breadcrumb style

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .breadcrumb-container
   ul.crumbs
-    li.crumbs__item(
+    li.crumb(
       v-for="(item, index) in items"
       v-if="item.length > 1"
       :key="index"
@@ -9,7 +9,7 @@
     )
       nuxt-link(
         :to="path(index)"
-      ).crumbs__item__link {{ prettyCrumb(item) }}
+      ).crumb__link {{ prettyCrumb(item) }}
 </template>
 
 <script>
@@ -66,17 +66,29 @@ export default {
     padding 0
     list-style-type none
 
-    &__item
-      background #fff
-      height 1.875em
-      display inline-flex
-      justify-content center
-      align-items center
-      padding 0.375em
-      margin 0.5em 0 0 0.5em
+  .crumb
+    background #fff
+    height 2em
+    display inline-flex
+    justify-content center
+    align-items center
+    padding .5em .375em .5em .5em
+    position relative
 
-      &__link
-        color #19175B
-        font-size 0.875em
-        font-weight bold
+    &:after
+      content ' '
+      border 1em solid transparent
+      border-left .5em solid white
+      height 1em
+      position absolute
+      right -1.5em
+      z-index 9999
+
+    &__link
+      color #19175B
+      font-size 0.875em
+      font-weight bold
+
+    + .crumb
+      margin-left .75em
 </style>
