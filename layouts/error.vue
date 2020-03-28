85b29c0b943c0577f060a950325c2f613e4a5ba1
IndiaSmile :) <dev@indiasmile.org>
2020-03-28 23:54:03 +0530

    feat add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .article
+    Header
+
+    .content(v-if="error.statusCode === 404")
+      h2.content__title Content Not Available! 😳
+
+      p.content__text
+        | We adding content as fast as possible! Please check again in a few hours to access this.
+
+      nuxt-link(to="/").content__heading Return to Home ↩️
+
+      b-message.message
+        | If you would like to help making this available faster, join our team! Email us at #[a(href="hi@indiasmile.org") hi@indiasmile.org] 🤓
+</template>
+
+<script>
+import Header from '~/components/Header'
+
+export default {
+  components: {
+    Header,
+  },
+  props: ['error'],
+
+  head() {
+    return {
+      title: 'Content Not Available',
+    }
+  },
+}
+</script>
+
+<style lang="stylus">
+.content
+  margin-top 4rem
+
+.message
+  margin-top 1rem
+</style>
