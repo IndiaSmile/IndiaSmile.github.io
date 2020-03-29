da791e167504916a491ef165aba3d1af016087f5
IndiaSmile :) <dev@indiasmile.org>
2020-03-29 19:03:59 +0530

    feat add ratings and links bollywood

--- pages/entertainment/movies/hindi/crime.vue
+++ pages/entertainment/movies/hindi/crime.vue
@@ -44,66 +44,118 @@ export default {
           text: `A pregnant woman travels to Kolkata from London to search for her missing husband. When all clues lead to a dead end, she realises that there is more than what meets the eye. The story totally grips you and leaves you at a dramatic high. Vidya Balan is amazing as always!`,
           rating: 8.1,
           image: require('~/assets/images/crime-kahani.jpg'),
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/70236020',
+            },
+          ],
         },
         {
           heading: 'A Wednesday',
           text: `A retired police commissioner recounts the most memorable case of his career wherein he was informed about a bomb scare in Mumbai by an ordinary commoner. Now this movie comes as a complete surprise and an extremely pleasant surprise I must say! Newer stories are slowly finding their way into cinema. A Wednesday is an intelligent diatribe against terrorism and a must watch!`,
           rating: 8.1,
           image: require('~/assets/images/crime-a-wednesday.jpg'),
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/70107499',
+            },
+          ],
         },
         {
           heading: 'Drishyam',
           text: `When the disappearance of a policewoman's son threatens to ruin Vijay’s family, he leaves no stone unturned in order to shield his family. A suspenseful drama with a nail-biting finish, it holds the viewer by the eyeballs till its' engaging climax.`,
           rating: 8.2,
           image: require('~/assets/images/crime-drishyam.jpg'),
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/80068117',
+            },
+          ],
         },
         {
           heading: 'Special 26',
           text: `A team of tricksters pose as CBI officers and conduct raids to rob politicians and businessmen of their black money. With the real CBI on their trail, they decide to pull off their biggest robbery. The film grips, excites and climaxes too! Catch it for a pure cinematic journey.`,
           rating: 8.0,
           image: require('~/assets/images/crime-special-26.jpg'),
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/70273637',
+            },
+          ],
         },
         {
           heading: 'Omkara',
           text: `Langda dreams of becoming Omkara's successor after the latter wins the election. But when Omkara appoints Kesu as his lieutenant, Langda feels betrayed and plots revenge. An extraordinary movie! The Actors were praised for their performances and still today, is an interesting watch.`,
           rating: 8.1,
           image: require('~/assets/images/crime-omkara.jpg'),
+          message: 'Available on Indian streaming platforms',
         },
         {
           heading: 'Raees',
           text: `Threat lurks over Raees Alam and his illicit business after the ACP decides to get the better of him. In order to survive and keep his trade thriving, Raees must overcome this. Shah Rukh Khan has never looked better, he is full of fury and for once, isn’t spreading his arms, but breaking others!`,
           rating: 6.8,
           image: require('~/assets/images/crime-raees.jpg'),
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/80164778',
+            },
+          ],
         },
         {
           heading: 'Satya',
           text: `Durga's brother is murdered in a gang war, turning him into a revenge-seeking man. Now, his ex-girlfriends cop husband is behind him. This movie has achieved a cult status, and is cited as one of the best Indian gangster films for its realistic portrayal of violence.`,
           rating: 8.2,
           image: require('~/assets/images/crime-satya.jpg'),
+          buttons: [
+            {
+              type: 'Prime Video',
+              link:
+                'https://www.amazon.com/Satya-J-D-Chakravarthy/dp/B07T1M3R51',
+            },
+          ],
         },
         {
           heading: 'Sarkar',
           text: `When a powerful gangster is framed for murder, his son steps up to protect his legacy. A gripping tale, this is Ram Gopal Varma at his finest!`,
           rating: 7.6,
           image: require('~/assets/images/crime-sarkar.jpg'),
+          message: 'Available on Indian streaming platforms',
         },
         {
           heading: 'Ab Tak Chhappan',
           text: `Sadhu, an encounter specialist, is armed with a personal vendetta to hunt and kill the gang responsible for his wife's death. Have to say, excellent camera work and great direction!`,
           rating: 7.9,
           image: require('~/assets/images/crime-ab-tak-chhappan.jpg'),
+          message: 'Available on Indian streaming platforms',
         },
         {
           heading: 'Shootout at Lokhandwala',
           text: `An experienced policeman, Khan, sets out for a shoot-out of gangsters in a residential locality of Mumbai.Little does he know that this will lead to his own integrity being questioned. Based on a true story, Shootout at Lokhandwala is a fantastic movie that is full of violence and naughty action.`,
           rating: 7.1,
           image: require('~/assets/images/crime-shootout-at-lokhandwala.jpg'),
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/70073018',
+            },
+          ],
         },
         {
           heading: 'Paan Singh Tomar',
           text: `Paan Singh Tomar, an athlete, is a gold medallist at the Indian National Games seven times in a row. He is forced to become a dacoit when his mother is murdered and the police don't take any action. The movie works because Irfan makes Paan Singh Tomar come alive as an athlete, as a husband and as a dacoit. This is a flawless performance.`,
           rating: 8.2,
           image: require('~/assets/images/crime-paan-singh-tomar.jpg'),
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/70123126',
+            },
+          ],
         },
       ],
     }
--- pages/entertainment/movies/hindi/horror.vue
+++ pages/entertainment/movies/hindi/horror.vue
@@ -39,6 +39,13 @@ export default {
           heading: '1920',
           text: `Arjun falls in love with Lisa and abandons his family as well as faith for her. They move to a bungalow, but when Lisa gets possessed by a demonic spirit, Arjun tries his best to save her. Second half of the movie is intense and the movie comprises of some truly artistic scenes!`,
           image: require('~/assets/images/horror-1920.jpg'),
+          rating: 6.4,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/70117674',
+            },
+          ],
         },
         {
           custom: 'Sharer',
@@ -47,46 +54,103 @@ export default {
           heading: 'Bhoot',
           text: `A bereaved shipping officer must save a girl he believes is real, on a stranded, unmanned haunted ship! While the film’s runtime is short, scenes are made as such so as to leave you waiting for the big reveal.`,
           image: require('~/assets/images/horror-bhoot.jpg'),
+          rating: 6.2,
+          message: 'Available on Indian streaming platforms',
         },
         {
           heading: 'Raaz',
           text: `Sanjana and Aditya go to Ooty for a holiday to save their unsteady marriage. However, Sanjana is haunted by a spirit who reveals some dark secrets about Aditya and his past. Raaz is an interesting mixture of thriller and romance. A good watch!`,
           image: require('~/assets/images/horror-raaz.jpg'),
+          rating: 6.5,
+          buttons: [
+            {
+              type: 'Prime Video',
+              link: 'https://www.amazon.com/Raaz-Dino-Morea/dp/B06WW3LPD8',
+            },
+          ],
         },
         {
           heading: 'Darna Mana Hai',
           text: `Six friends narrate spooky tales to one another while stranded on a highway. Each story turns out to be scarier than the next one but the biggest horror strikes when some of the friends go missing. Darna Mana Hai is a ghost movie having 6 stories in one story. Some of the stories are really scary and the plot seems to have an overall compelling effect of fear.`,
           image: require('~/assets/images/horror-darna-mana-hai.jpg'),
+          rating: 6.3,
+          buttons: [
+            {
+              type: 'Prime Video',
+              link:
+                'https://www.primevideo.com/detail/amzn1.dv.gti.b8b4041b-edd6-49f6-9a02-19c550f9deea/?ref_=dvm_pds_gen_in_as_s_gt_dtites1690B3|c_413926075644_m_XxHIU7ZL-dc_s_',
+            },
+          ],
         },
         {
           heading: 'Raat',
           text: `When a family moves into their new house in the city, they start experiencing strange incidents around the house. Soon, they seek the help of a priest when their daughter gets possessed. The movie establishes an aura and is intense at several places. Revathi is brilliant and truly convincing in her efforts.`,
           image: require('~/assets/images/horror-raat.jpg'),
+          rating: 7.1,
+          message: 'Available on Indian streaming platforms',
         },
         {
           heading: '13B: Fear Has a New Address',
           text: `Manohar, along with his family, moves into a new apartment on the 13th floor. However, their peace is disturbed by a spirit that uses the TV as a medium to communicate with them. Watch '13B' for some smart cinematography, a stylised monochrome tint and some unique editing.`,
           image: require('~/assets/images/horror-13-b.jpg'),
+          rating: 6.3,
+          buttons: [
+            {
+              type: 'hotstar',
+              link: 'https://www.hotstar.com/in/movies/13b/1000214941',
+            },
+          ],
         },
         {
           heading: 'Stree',
           text: `The people of a village live under a constant fear of Stree, the spirit of a woman, who attacks men at night during the festivals. Vicky, along with his friends, decides to solve the mystery. Stree is an entertaining film. An experimental comedy, that creates an eerie atmosphere and it manages to be funny and scary at the same.`,
           image: require('~/assets/images/horror-stree.jpg'),
+          rating: 7.6,
+          buttons: [
+            {
+              type: 'Netflix',
+              link: 'https://www.netflix.com/title/81113921',
+            },
+          ],
         },
         {
           heading: 'Tumbbad',
           text: `When a family builds a shrine for Hastar, a monster who is never to be worshipped, and attempts to get their hands on his cursed wealth, they face catastrophic consequences. Tumbbad is a moody and a unique masterpiece. This one is genuinely scary.`,
           image: require('~/assets/images/horror-tumbbad.jpg'),
+          rating: 8.3,
+          buttons: [
+            {
+              type: 'Prime Video',
+              link:
+                'https://www.primevideo.com/detail/Tumbbad/0JP2YUWYYUI1Q2NN73XBXXXC2T?ext_vrnc=hi&ref_=dvm_pds_gen_in_as_s_gt_dsastandardadgroups',
+            },
+          ],
         },
         {
           heading: 'Pari',
           text: `A kind-hearted man tries to help a chained woman in a hut who is probably a victim of abuse. However, he soon realises that things are not as they appear to be. Pari manages to frighten you purely with its superlative performances and spine-chilling visuals.`,
           image: require('~/assets/images/horror-pari.jpg'),
+          rating: 6.6,
+          buttons: [
+            {
+              type: 'Prime Video',
+              link:
+                'https://www.primevideo.com/detail/0IV864SLW83K2KCA2LWGBULRC6/?ref_=dvm_pds_gen_in_as_s_gt_gobig98desktop&ext_vrnc=hi|c_362587654941_m_qOZ8LHIi-dc_s_',
+            },
+          ],
         },
         {
           heading: 'Bhool Bhulaiyaa',
           text: `An NRI and his wife decide to stay in his ancestral home, paying no heed to the warnings about ghosts. Soon, inexplicable occurrences cause him to call a psychiatrist to help solve the mystery. One Excellent masterpiece. This is one of the rare movies where the story can be interpreted in multiple ways! A must watch, the film is hilarious and you can simply confuse it for a hardcore comedy in the start.`,
           image: require('~/assets/images/horror-bhool-bhulaiya.jpg'),
+          rating: 7.3,
+          buttons: [
+            {
+              type: 'Netflix',
+              link:
+                'https://www.primevideo.com/region/eu/detail/0N4MPYB5ZMT5FTXJWWKQU4KRDZ/ref_=dvm_pds_gen_in_as_s_gt_top1000dsad412?ext_vrnc=hi%7Cc_414533225917_m_pAec9xcy-dc_s_',
+            },
+          ],
         },
       ],
     }
