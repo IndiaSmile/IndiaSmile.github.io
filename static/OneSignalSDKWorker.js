664252a53de6e063da6f522c4d8cbb888f76d99e
IndiaSmile :) <dev@indiasmile.org>
2020-04-11 14:59:31 +0530

    fix use universal storage module

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,4 +1,1 @@
-importScripts(
-  '/sw.js?1586596028076',
-  'https://cdn.onesignal.com/sdks/OneSignalSDK.js'
-)
+importScripts('/sw.js?1586596399681', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
