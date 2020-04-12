f8d32450ba81587c9113746d97d2607a618344e3
IndiaSmile :) <dev@indiasmile.org>
2020-04-12 16:47:20 +0530

    attempt new netlify

--- components/Tracker.vue
+++ components/Tracker.vue
@@ -47,7 +47,7 @@ export default {
     return {
       position: null,
       distance: null,
-      endpoint: '/api-geo2covid/',
+      endpoint: '/api-geo2covid/exec',
 
       showError: false,
     }
